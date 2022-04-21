9f533c5a18dc86935a8dbb7e286584ca861b97ad
VanTrung0307 <trunglelop95@gmail.com>
2022-04-21 19:40:06 +0700

    State

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,9 +1,43 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+
+    //key:value
+    state = {
+        name: 'Trung',
+        channel: 'ReactJS Basic'
+    }
+
+    /*
+    JSX => return block
+    fragment
+    */
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
     render() {
+        //let name = 'Trung';
+
+
         return (
-            <div>Hello my component Van Trung</div>
+            /* có thể dùng <> </> (shortcut) để bọc ngoài nhiều khối html trong react
+                <></> cũng là 1 fragment trong react*/
+            <React.Fragment>
+                <div className="first">
+                    {/* {console.log('My name is:', name)} */}
+                    <input value={this.state.name} type="text"
+                        onChange={(event) => this.handleOnChangeName(event)}
+                    />
+                    Hello my component, My name is {this.state['name']}
+                </div>
+                <div className="second">
+                    My lesson : {this.state.channel}
+                </div>
+            </React.Fragment>
         )
     }
 }
